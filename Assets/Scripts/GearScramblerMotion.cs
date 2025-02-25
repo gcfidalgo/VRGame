e9a7ad5e9b17cb1a7d61b1ab420adcd669681db2
gcfidalgo <gc.fidalgo@gmail.com>
2025-02-25 11:50:46 -0600

    Environment

---
+++ Assets/Scripts/GearScramblerMotion.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearScramblerMotion : MonoBehaviour
+{
+    private Vector3 myStartPosition;
+    private AudioSource myAudioSource;
+    private MeshRenderer myMeshRenderer;
+    private CapsuleCollider myCapsuleCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myStartPosition = transform.position;
+        myAudioSource = GetComponent<AudioSource>();
+        myMeshRenderer = GetComponent<MeshRenderer>();
+        myCapsuleCollider = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = myStartPosition + new Vector3(0.0f, Mathf.Sin(Time.time) / 3.0f, 0.0f);
+        //transform.Rotate(Vector3.forward * 50 * Time.deltaTime, Space.Self);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        myAudioSource.Play();
+        myMeshRenderer.enabled = false;
+        myCapsuleCollider.enabled = false;
+    }
+}
+
